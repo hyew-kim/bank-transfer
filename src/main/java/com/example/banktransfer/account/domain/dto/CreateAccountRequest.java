c208b2b93124d868a5275a04e3bac96f0a241862
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2025-12-22 22:06:24 +0900

    feat: api description 작성

--- src/main/java/com/example/banktransfer/account/domain/dto/CreateAccountRequest.java
+++ src/main/java/com/example/banktransfer/account/domain/dto/CreateAccountRequest.java
@@ -3,11 +3,15 @@ package com.example.banktransfer.account.domain.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 public record CreateAccountRequest(
         @NotNull Long  userId,
+        @Length(min = 3, max = 3, message = "은행코드는 3자리 입력하셔야 합니다.")
         @NotEmpty String bankCode,
-        @NotEmpty String accountNumber,
+        @NotEmpty
+        @Length(min = 14, max = 14, message = "계좌번호는 14자리 입력하셔야 합니다.")
+        String accountNumber,
         @NotEmpty String holderName
 ) {
     public static CreateAccountRequest of(Long userId, String bankCode, String accountNumber, String holderName) {
